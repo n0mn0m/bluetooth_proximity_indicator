38bf3af835546474c8dd50c32a4687b38e119e65
n0mn0m <alexander@unexpectedeof.net>
2020-01-25 19:13:59 -0500

    Switch to scanning the local area.
    Switch to a scan and count of the local area instead of using the notification to make the sensing more idependent.

--- code.py
+++ code.py
@@ -1,17 +1,15 @@
 """
-For use with the CircuitPlayground Bluefruit. Polls for a ble connection
-with an Apple device. Once paired checks for new SMS notifications that
-contain a valid color. If a valid color is provided the playground neopixels
-are set. If a connection is not detected within 15 minutes the playground will
-enter a temporary sleep and restart the cycle.
+For use with the CircuitPlayground Bluefruit. Polls the count of
+local connectable devices and based on the count changes pixel colors.
 """
 
 import time
-import board
-import neopixel
+
 import adafruit_ble
-from adafruit_ble.advertising.standard import SolicitServicesAdvertisement
-from adafruit_ble.services.apple import AppleNotificationService
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising import Advertisement
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_circuitplayground.bluefruit import cpb
 
 # NEOPIXEL RGB values
 COLORS = {
@@ -32,9 +30,8 @@ def color_chase(color, wait):
     Rotate through the neopixels setting the new color.
     """
     for i in range(10):
-        pixels[i] = color
+        cpb.pixels[i] = color
         time.sleep(wait)
-        pixels.show()
     time.sleep(0.5)
 
 def rainbow(wait=.1):
@@ -42,10 +39,9 @@ def rainbow(wait=.1):
     Cycle through the RGB spectrum and set the new neopixel color.
     """
     for j in range(255):
-        for i in range(len(pixels)):
+        for i in range(len(cpb.pixels)):
             idx = int(i + j)
-            pixels[i] = wheel(idx & 255)
-        pixels.show()
+            cpb.pixels[i] = wheel(idx & 255)
         time.sleep(wait)
 
 def wheel(pos):
@@ -62,61 +58,70 @@ def wheel(pos):
     pos -= 170
     return (pos * 3, 0, 255 - pos * 3)
 
-radio = adafruit_ble.BLERadio()
-a = SolicitServicesAdvertisement()
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.1, auto_write=True)
 
-a.solicited_services.append(AppleNotificationService)
-radio.start_advertising(a)
-
-known_notifications = set()
-found_color_count = 0
-sleeping = 0
-
-DEFAULT_COLOR = "PURPLE"
-LAST_COLOR = None
-
-color_chase(COLORS[DEFAULT_COLOR], wait=.5)
-
-while not radio.connected:
+def local_scan(timeout=2):
     """
-    If we are not connected sleep for 6 seconds unless
-    it's been 15 minutes. After 15 minutes do a rainbow
-    cycle and go to sleep for 5 minutes.
+    Find out how many devices are in the area that we can
+    connect to.
     """
-    if sleeping == 150:
-        rainbow()
+    print("\nscanning\n")
+    found = set()
+    resp = set()
+
+    for ad in ble.start_scan(Advertisement, timeout=timeout):
+        # dir(ad)
+        # ['__bytes__', '__class__', '__dict__', '__init__', '__len__',
+        # '__module__', '__qualname__', '__repr__', '__str__', 'address',
+        # 'connectable', 'matches', 'rssi', 'scan_response', 'tx_power',
+        # 'complete_name', 'from_entry', 'prefix', 'data_dict', 'flags',
+        # 'mutable', 'short_name', 'appearance', '_rssi']
+        #
+        # Most of the name related fields just return None
+        resp.add(ad.address)
+        if ad.connectable:
+            print(ad.__dict__)
+            found.add(ad.address)
+
+    print("\nscan done\n")
+    print("\ntotal: {total}\nconnectable: {found}\n".format(found=len(found), total=len(resp)))
+    return len(found)
+
+ble = BLERadio()
+cpb.pixels.brightness = 0.1
+FIRST = True
+scan_count = 0
+
+def deep_sleep(seconds):
+    """
+    Would be useful to be able to power down the radio
+    to save battery, but for now this is a wrapper
+    to indicate behavior to the reader.
+    """
+    print("\nGoing to sleep for {} seconds.\n".format(seconds))
+    time.sleep(seconds)
+
+
+while True:
+    if cpb.switch:
+        if FIRST:
+            rainbow(.01)
+            FIRST = False
+        while scan_count < 2:
+            scan_count += 1
+            found = local_scan()
+            if found == 0 or found < 2:
+                color_chase(COLORS["OFF"], wait=.5)
+            if found > 0 and found < 5:
+                color_chase(COLORS['RED'], wait=.5)
+            if found >= 5:
+                color_chase(COLORS['BLUE'], wait=.5)
+                color_chase(COLORS['PURPLE'], wait=.5)
+            time.sleep(5)
+        # Stop scanning for 15 minutes
+        deep_sleep(900)
+        scan_count = 0
+    else:
+        # Slide is off clear all pixels
         color_chase(COLORS["OFF"], wait=.5)
-        time.sleep(300)
-        sleeping == 0
-    time.sleep(6)
-    sleeping += 1
-    pass
-
-while radio.connected:
-    """
-    Check for SMS notifications on the paired device and set
-    the color of the neopixels on the circuitplayground if a
-    valid color is provided.
-    """
-    for connection in radio.connections:
-        if not connection.paired:
-            connection.pair()
-            if LAST_COLOR:
-                color_chase(LAST_COLOR, wait=.5)
-            sleeping = 0
-
-        ans = connection[AppleNotificationService]
-
-        for notification in ans.wait_for_new_notifications():
-            if notification.app_id == "com.apple.MobileSMS"and not notification.preexisting:
-                if notification.message.upper() in COLORS.keys():
-                    if found_color_count == 2:
-                        rainbow(.01)
-                        found_color_count = 0
-                    color_chase(COLORS[notification.message.upper()], wait=.5)
-                    found_color_count += 1
-                    LAST_COLOR = COLORS[notification.message.upper()]
-
-    time.sleep(3)
-
+        FIRST = True
+        scan_count = 0
